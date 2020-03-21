cf704eaca9efb6ad409de438de9d0754b360f85b
Franco Benitez <franco_casla_benitez@hotmail.com>
2020-03-21 11:09:02 -0300

    pantalla de pregunta lista + masInformacion

--- src/app/components/puntajes/puntajes.component.ts
+++ src/app/components/puntajes/puntajes.component.ts
@@ -11,11 +11,11 @@ export class PuntajesComponent implements OnInit {
   public usuarios:any = [
     { nombre:'Franco', apellido:'Benitez', puntos:'1885', imagen:'https://scontent-eze1-1.xx.fbcdn.net/v/t1.0-9/45401820_1593472890798376_2447605581708525568_n.jpg?_nc_cat=107&_nc_sid=85a577&_nc_ohc=66OAMUQgZAcAX_7drWJ&_nc_ht=scontent-eze1-1.xx&oh=19f79365e883770abf6f79535130e84d&oe=5E904745'
     },
-    { nombre:'Jorge', apellido:'Mendoza', puntos:'1845', imagen:'https://www.facebook.com/search/async/profile_picture/?fbid=100001977790768&width=72&height=72'
+    { nombre:'Jorge', apellido:'Mendoza', puntos:'1845', imagen:'https://scontent-eze1-1.xx.fbcdn.net/v/t1.0-9/s960x960/75472833_2528623940546841_8974005797562351616_o.jpg?_nc_cat=109&_nc_sid=85a577&_nc_ohc=C7zYv96Vpq4AX96eqF3&_nc_ht=scontent-eze1-1.xx&_nc_tp=7&oh=435e8ce73a35c9e76022b01ea43cbf72&oe=5E96C15A'
     },
-    { nombre:'Andrés', apellido:'Obregon', puntos:'1145', imagen:'https://www.facebook.com/search/async/profile_picture/?fbid=100000193045407&width=72&height=72'
+    { nombre:'Andrés', apellido:'Obregon', puntos:'1145', imagen:'https://scontent-eze1-1.xx.fbcdn.net/v/t1.0-9/82343958_3246177295398673_1112499522979758080_n.jpg?_nc_cat=110&_nc_sid=85a577&_nc_ohc=JKHL-VS0oKQAX-r-B0d&_nc_ht=scontent-eze1-1.xx&oh=318a284653947022d92b8268dbd342a3&oe=5E965282'
     },
-    { nombre:'Roberto', apellido:'Alvarez', puntos:'879', imagen:'https://www.facebook.com/search/async/profile_picture/?fbid=100000259301993&width=72&height=72'
+    { nombre:'Roberto', apellido:'Alvarez', puntos:'879', imagen:'https://scontent-eze1-1.xx.fbcdn.net/v/t1.0-9/13522064_1218400721511893_6360243226174226848_n.jpg?_nc_cat=110&_nc_sid=85a577&_nc_ohc=sZUnVmQEOaIAX_Iky4G&_nc_ht=scontent-eze1-1.xx&oh=0e478f7843b1f20256c0e7d47e5efd8a&oe=5E97EA2B'
     }
   ];
 
--- src/app/pregunta/pregunta.page.ts
+++ src/app/pregunta/pregunta.page.ts
@@ -13,12 +13,60 @@ export class PreguntaPage implements OnInit {
   }
 
   public categoria = this.activatedRoute.snapshot.paramMap.get('categoria');
+  public verInfo = false;
+  public time = 15;
+  public intervalo;
+
+  public showRespuestaIncorrecto = false;//para mostrar 'correcto o incorrecto' cuando clickee sobre una opcion
+  public showRespuestaCorrecto = false;
+  public respuestaStatus:string; //correcto o incorrecto
+
+  reloj() {
+    this.time = this.time - 1;
+  }
+
+  respuesta(respuesta,respuestaCorrecta){
+    clearInterval(this.intervalo);
+    if (respuesta == respuestaCorrecta) {
+      this.showRespuestaCorrecto = true;
+      this.respuestaStatus = 'Correcto';
+      // let alertRespuesta = document.getElementById('respuesta');
+      // alertRespuesta.classList.add('correcto');//le agrego la clase correcto para que se vea en verde
+      setTimeout(() => {
+        this.showInfo();
+      }, 1500);
+      return true;
+    }
+    this.showRespuestaIncorrecto = true;
+    this.respuestaStatus = 'Incorrecto';
+    // let alertRespuesta = document.getElementById('respuesta');
+    // alertRespuesta.classList.add('incorrecto')//le agrego la clase incorrecto para que se vea en rojo
+    setTimeout(() => {
+      this.showInfo();
+    }, 1500);
+    return false;
+  }
+
+  showInfo(){
+    this.showRespuestaCorrecto = false;
+    this.showRespuestaIncorrecto = false;
+    let divRespuestas = document.getElementById('respuestas');
+    divRespuestas.classList.add('ocultarRespuesta');
+    setTimeout(() => {
+      this.verInfo = true;
+    },500);
+  }
+
   ngOnInit() {
-  }
+    //descontar un segundo en el cronometro
+    this.intervalo = setInterval(()=>{
+      this.reloj()
+    },1000)
 
-  respuesta(){
-    console.log('click');
-    
+    //en el caso de que no haya contestado a tiempo, destruyo ese temporizador
+    setTimeout(() => {
+      clearInterval(this.intervalo);
+      this.showInfo();
+    }, 15500);
   }
-
 }
